611c39f1adb0746e0bcbeafebdf44fc24572e606
Irvile Rodrigues <irvile.rodrigues@gmail.com>
2021-08-14 15:07:13 -0300

    chore: config shipjs

---
+++ ship.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  monorepo: {
+    mainVersionFile: 'lerna.json',
+    // We rely on Lerna to bump our dependencies.
+    packagesToBump: [],
+    packagesToPublish: ['packages/tordojs'],
+  },
+  publishCommand({ tag }) {
+    return `yarn publish --access public --tag ${tag}`
+  },
+  versionUpdated({ exec, version }) {
+    // Update package dependencies
+    exec(`yarn lerna version ${version} --exact --no-git-tag-version --no-push --yes`)
+
+    // Ship.js reads JSON and writes with `fs.writeFileSync(JSON.stringify(json, null, 2))`
+    // which causes a formatting error in the `lerna.json` file.
+    exec('yarn prettier lerna.json --write')
+  },
+  // Skip preparation if it contains only `chore` commits
+  shouldPrepare({ releaseType, commitNumbersPerType }) {
+    const { fix = 0 } = commitNumbersPerType
+
+    return releaseType !== 'patch' || fix !== 0
+  },
+}
